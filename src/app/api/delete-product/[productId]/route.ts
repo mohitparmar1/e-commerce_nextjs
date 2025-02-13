6bc78f8a54505ad526516a3391fad6318b0f934e
Mohit Parmar <100152343+mohitparmar1@users.noreply.github.com>
2025-02-13 21:46:34 +0530

    delete-product route

---
+++ src/app/api/delete-product/[productId]/route.ts
@@ -0,0 +1,63 @@
+// app/api/delete-product/[productId]/route.ts
+import dbConnect from "@/app/utils/dbConnect";
+import { Product } from "@/app/model/User.Model";
+import mongoose from "mongoose";
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { productId: string } }
+) {
+    await dbConnect();
+    try {
+        const { productId } = await params;
+
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+            console.error("Invalid product ID format:", productId);
+            return Response.json(
+                {
+                    success: false,
+                    message: "Invalid product ID format",
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+
+
+        const product = await Product.findByIdAndDelete(productId);
+
+        if (!product) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Product not found",
+                },
+                {
+                    status: 404,
+                }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Product deleted successfully",
+            },
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error deleting product",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
